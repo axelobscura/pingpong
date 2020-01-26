cf673d132cd94e7b1af86f6d0556bf2f7cf09f9c
Axel <axosar@gmail.com>
2020-01-26 16:28:46 -0600

    Tipografia

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>PINGPONG</h1>
+        <h1>PING-PONG</h1>
       </header>
     </div>
   );
