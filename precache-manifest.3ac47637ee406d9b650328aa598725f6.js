b8b94283205f089365d814c6849739f933eba08a
Axel <axosar@gmail.com>
2020-01-26 15:14:36 -0600

    Updates

---
+++ precache-manifest.3ac47637ee406d9b650328aa598725f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edadb6264fb0fd3e703993fbe1298d55",
+    "url": "/pingpong/index.html"
+  },
+  {
+    "revision": "7dec45c0c0f28934d7b0",
+    "url": "/pingpong/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5086f2162ae28b535101",
+    "url": "/pingpong/static/js/2.bb741e0d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pingpong/static/js/2.bb741e0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7dec45c0c0f28934d7b0",
+    "url": "/pingpong/static/js/main.9d4dc0e9.chunk.js"
+  },
+  {
+    "revision": "b009f4b7bf8020840432",
+    "url": "/pingpong/static/js/runtime-main.5de3e6c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pingpong/static/media/logo.5d5d9eef.svg"
+  }
+]);
