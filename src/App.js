5961ebf1a76fce339279b86ec1e3d2dd84bc49e7
Axel <axosar@gmail.com>
2020-01-28 22:28:42 -0600

    Cambio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount(count + 1)
+    }, 5000);
+  })
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1>PING-PONG</h1>
+        {count}
       </header>
     </div>
   );
